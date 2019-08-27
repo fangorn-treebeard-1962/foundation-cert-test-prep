0b31cda9c75efbd2f6b5152d6cfda910a3454ab5
Pete Lund <plunder62@yahoo.com>
2019-08-27 10:31:05 -0500

    added answer mode for terms

--- quizzer.py
+++ quizzer.py
@@ -1,6 +1,22 @@
 from copy import deepcopy
 from random import choice, choices, shuffle
 from os import system
+import getch
+
+
+def main():
+    print('(Q)uestion or (T)erms?', end=' ')
+    quiz_type = getch.getche()
+    num_terms = 4  # len(term_dict)
+
+    if quiz_type.lower() == 't':
+        print('\n(Q)uestion or (A)nswer mode?', end=' ')
+        term_mode = getch.getche()
+        term_dict = load_terms()
+        do_term_quiz_with_hints(term_dict, num_terms, term_mode == 'a')
+    else:
+        questions_dict = load_questions()
+        do_question_quiz(questions_dict, num_terms)
 
 
 def load_questions():
@@ -31,15 +47,6 @@ def load_terms():
     return term_dict
 
 
-def main():
-    term_dict = load_terms()
-    num_terms = 4  # len(term_dict)
-    # do_term_quiz_with_hints(term_dict, num_terms)
-
-    questions_dict = load_questions()
-    do_question_quiz(questions_dict, num_terms)
-
-
 def do_question_quiz(dict, num_terms, answer_mode = False):
     questions_dict = deepcopy(dict)
     success_count, failure_count = 0, 0
@@ -50,26 +57,33 @@ def do_question_quiz(dict, num_terms, answer_mode = False):
         print('Questions Quiz ({count}/{total})\n'.format(count=(i + 1), total=num_terms))
         q = choice(list(questions_dict.keys()))
         answer = questions_dict.pop(q)
-        input("{question} ".format(question=q))
-        print("{value}".format(value=answer))
-        input_value = input("did you get it right? ")
-        if input_value == 'y':
+        print("{question} ".format(question=q), end=' ')
+        getch.getch()
+
+        print("\n\n{value}".format(value=answer))
+        print("did you get it right? ", end=' ')
+        input_value = getch.getch()
+        if input_value.lower() == 'y':
             success_count = success_count + 1
             # msg = "Correct!"
         else:
-            input("whoops!  correct answer was \n {answer}".format(answer=answer))
+            # input("whoops!  correct answer was \n {answer}".format(answer=answer))
             failure_count = failure_count + 1
             failure_dict[q] = answer
         # input("{msg} ".format(msg=msg))
 
+    report_results(success_count, failure_count, failure_dict)
+
+    return success_count, failure_count, failure_dict
+
+
+def report_results(success_count, failure_count, failure_dict):
     system('clear')
     print("Quizzing Complete:\n{right} right, {wrong} wrong\n".format(right=success_count, wrong=failure_count))
     if failure_count > 0:
         print('You missed\n')
         for key in failure_dict.keys():
-            print("\t{key}): {answer}".format(key=key, answer=failure_dict[key]))
-
-    return success_count, failure_count, failure_dict
+            print("\t*** {key}\n{answer}".format(key=key, answer=failure_dict[key]))
 
 
 def do_term_quiz(term_dict):
@@ -105,11 +119,19 @@ def build_possible_answers(all_answers, answer, num_hints):
     return answers, answers.index(answer) + 1
 
 
-def do_term_quiz_with_hints(dict, num_terms):
-    term_dict = deepcopy(dict)
-    all_answers = list(load_terms().values())
+def do_term_quiz_with_hints(dict, num_terms, answer_mode=False):
     success_count, failure_count = 0, 0
     failure_dict = {}
+
+    if answer_mode:
+        # reverse the dictionary
+        term_dict = {}
+        for (k,v) in dict.items():
+            term_dict[v] = k
+    else:
+        term_dict = deepcopy(dict)
+
+    all_answers = list(term_dict.values())
 
     for i in range(num_terms):
         system('clear')
@@ -118,7 +140,11 @@ def do_term_quiz_with_hints(dict, num_terms):
         # pick a random term
         term = choice(list(term_dict.keys()))
         answer = term_dict.pop(term)
-        input("what is the definition of {key}? ".format(key=term))
+        if answer_mode:
+            print("what term represents:\n\t{key}? ".format(key=term))
+        else:
+            print("what is the definition of {key}? ".format(key=term))
+        getch.getch()
 
         num_hints = 4
         possible_answers, correct_answer = build_possible_answers(all_answers, answer, num_hints)
@@ -129,22 +155,19 @@ def do_term_quiz_with_hints(dict, num_terms):
                 ans = '[undefined]'
             print("{offset}): {value}".format(offset=(index + 1), value=ans))
 
-        input_value = input("Answer? (1-{num_answers}) ".format(num_answers=(num_hints + 1)))
+        print("Answer? (1-{num_answers}) ".format(num_answers=(num_hints + 1)), end=' ')
+        input_value = getch.getche()
         if int(input_value) == correct_answer:
             success_count = success_count + 1
-            msg = "Correct!"
+            # msg = "Correct!"
         else:
             msg = "whoops!  correct answer was \n{num}): {answer}".format(num=correct_answer,answer=answer)
             failure_count = failure_count + 1
             failure_dict[term] = answer
-        input("{msg} ".format(msg=msg))
+            print("{msg} ".format(msg=msg))
+            getch.getch()
 
-    system('clear')
-    print("Quizzing Complete:\n{right} right, {wrong} wrong\n".format(right=success_count, wrong=failure_count))
-    if failure_count > 0:
-        print('You missed\n')
-        for key in failure_dict.keys():
-            print("\t{key}): {answer}".format(key=key, answer=failure_dict[key]))
+    report_results(success_count, failure_count, failure_dict)
 
     return success_count, failure_count, failure_dict
 
